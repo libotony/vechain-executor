56cb68c009f0a4e55d55af065b79855dba7abf16
tony <liboliqi@gmail.com>
2025-07-01 20:53:57 +0800

    update node url

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 const thor = new Connex.Thor({
-    node: 'https://sync-mainnet.veblocks.net',
+    node: 'https://mainnet.vechain.org',
     network: 'main'
 })
 const vendor = new Connex.Vendor('main', 'sync2')
